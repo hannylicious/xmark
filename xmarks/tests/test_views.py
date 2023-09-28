855adf99252fe6700369b383aa7404a6a429140d
H G <hannylicious@gmail.com>
2023-09-28 06:21:59 -0500

    Add view tests for logged-in / annonymous users for index view

--- xmarks/tests/test_views.py
+++ xmarks/tests/test_views.py
@@ -6,17 +6,24 @@ from users.models import User
 from xmarks.models import Bookmark
 
 
-class IndexViewTestCase(TestCase):
-    def test_index_view(self):
-        def call_index():
-            self.client.get(reverse("xmarks:index"))
+class ViewTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username="testuser", password="12345")
 
-        user = User.objects.create_user(username="testuser", password="12345")
-        bookmark = Bookmark.objects.create(
-            name="test bookmark", url="https://www.google.com", user=user
-        )
-        bookmarks = Bookmark.objects.all()
+    def call_index(self):
+        self.client.get(reverse("xmarks:index"))
+
+
+class IndexViewTestCase(ViewTestCase):
+    def test_index_view_redirects_non_authenticated_users(self):
         response = self.client.get(reverse("xmarks:index"))
         self.assertEqual(response.status_code, 302)
-        self.assertNumQueries(0, call_index)
-        self.assertContains(response, "Hello, world!")
+        self.assertNumQueries(0, self.call_index)
+        self.assertEqual(response.url, reverse("login") + "?next=/xmarks/")
+
+    def test_index_view_loads_for_authenticated_users(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("xmarks:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertNumQueries(3, self.call_index)
+        self.assertTemplateUsed(response, "xmarks/index.html")
