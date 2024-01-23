23529f8683d3cd413d2dc83d4ef888937419f351
H G <hannylicious@gmail.com>
2024-01-23 10:39:33 -0600

    Add reading of .env to wsgi.py

--- xmark/wsgi.py
+++ xmark/wsgi.py
@@ -6,11 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
-import os
-
+import environ
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xmark.settings.base")
+######
+# Environment
+######
+ROOT_DIR = environ.Path(__file__) - 2
+# Take environment variables from .env file
+environ.Env.read_env(ROOT_DIR(".env"))
 
+######
+# Django Application
+######
 application = get_wsgi_application()
