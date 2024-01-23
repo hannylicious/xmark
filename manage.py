b6fc1d5e79cbccd707465a2ff51e064bd12d5c98
H G <hannylicious@gmail.com>
2024-01-23 10:30:15 -0600

    Update the implementation of the .env files in manage.py to appropriately read them

--- manage.py
+++ manage.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import sys
-from pathlib import Path
 
 
-def main():
+def main() -> None:
     """Run administrative tasks."""
     import environ
 
-    root_dir = Path(__file__).parent
-    breakpoint()
-    environ.Env(
-        DJANGO_SETTINGS_MODULE=(str, "xmark.settings.base"),
-    )
+    root_dir = environ.Path(__file__) - 1
     # Take environment variables from .env file
-    environ.Env.read_env(root_dir / ".env")
+    environ.Env.read_env(root_dir(".env"))
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
