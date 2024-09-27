8664ba34d3cbbfe01a17ee18b265395b27194311
Hanny <me@hannylicious.com>
2024-09-27 13:34:36 -0500

    Make primary project url navigate to xmarks root url

--- xmark/urls.py
+++ xmark/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path(
@@ -30,5 +31,5 @@ urlpatterns = [
         name="logout",
     ),
     path("admin/", admin.site.urls),
-    path("xmarks/", include("xmarks.urls")),
+    path("", include("xmarks.urls")),
 ]
